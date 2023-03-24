9e3f230a90fe678472cf69ae5eef5299812e53e2
Mayank-141-Shaw <mayank141shaw@gmail.com>
2023-03-24 21:21:40 +0530

    Added the file controllers

--- Advanced-Algorithms/src/MergeSort.java
+++ Advanced-Algorithms/src/MergeSort.java
@@ -1,4 +1,12 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.Arrays;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 public class MergeSort {
 	
@@ -69,10 +77,55 @@ public class MergeSort {
     }
 
 	public static void main(String args[]) throws Exception{
-		int[] arr = { 45, 78, 21, 9, 34, 2, 19, 90, 8, 24, 21 };
+		int n = (int)Math.pow(2, 20);
 		
+		final String NUM_FILENAME = "../static/random_numbers.txt";
+		final String RESULT_FILENAME = "../static/mergesort_sorted_numbers.txt";
+		
+		// create an array of random numbers
+		Random rd = new Random();
+		int[] arr = new int[n];
+		
+		for(int i=0; i<n; i++) {
+			arr[i] = rd.nextInt(20000);
+		}
+		
+		// save this array in a file
+		File file = new File(NUM_FILENAME);
+		
+		BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+		bw.write( Arrays
+				.stream(arr)
+				.mapToObj(String::valueOf)
+				.collect(Collectors.joining(" "))
+				);
+		bw.close();
+		
+		
+		// collect array from the file
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		String line = br.readLine();	// array is in a single line
+		br.close();
+		
+		arr = Arrays
+				.stream(line.split(" "))		// splitting int string to array of string
+				.mapToInt(Integer::parseInt)	// converting string to int
+				.toArray();						// changing the stream to an array
+		
+		// sort this array
 		MergeSort ob = new MergeSort();
         ob.sort(arr, 0, arr.length - 1);
+        
+        // store the sorted array in a new sorted file
+        File resultFile = new File(RESULT_FILENAME);
+        bw = new BufferedWriter(new FileWriter(resultFile));
+        bw.write( Arrays
+        		.stream(arr)
+        		.mapToObj(String::valueOf)
+        		.collect(Collectors.joining(" "))
+        		);
+        bw.close();
+        
 		
 		Arrays.stream(arr).forEach(val -> System.out.print(val + " "));
 	}
